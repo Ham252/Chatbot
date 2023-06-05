2ac0d7f5fc49c85f16167932989fc6c449e69763
Hamza Amin <90584545+Ham252@users.noreply.github.com>
2023-06-05 17:16:50 +0300

    Update chat.py

--- chat.py
+++ chat.py
@@ -11,19 +11,17 @@ with open('intents.json', 'r') as json_data:
 
 FILE = "data.pth"
 data = torch.load(FILE)
-
 input_size = data["input_size"]
 hidden_size = data["hidden_size"]
 output_size = data["output_size"]
 all_words = data['all_words']
 tags = data['tags']
 model_state = data["model_state"]
-
 model = NeuralNet(input_size, hidden_size, output_size).to(device)
 model.load_state_dict(model_state)
 model.eval()
+bot_name = "Osama"
 
-bot_name = "Sam"
 print("Let's chat! (type 'quit' to exit)")
 while True:
     # sentence = "do you use credit cards?"
